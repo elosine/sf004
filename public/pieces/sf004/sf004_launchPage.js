3cb3f826fdc9c9a4a1a72588046525a0bfd501fa
elosine <justinwenloyang@gmail.com>
2021-07-17 18:18:59 -0400

    uu

---
+++ public/pieces/sf004/sf004_launchPage.js
@@ -0,0 +1,79 @@
+// <editor-fold> Panel Dimensions
+let w = 420;
+let h = 265;
+let center = w / 2;
+// </editor-fold> END Panel Dimensions
+
+// <editor-fold> Main Panel
+let panel = mkPanel({
+  title: 'Soundflow #4 - Launch',
+  onwindowresize: true,
+  w: w,
+  h: h
+});
+let canvas = panel.content;
+// </editor-fold> END Main Panel
+
+// <editor-fold> Title Text
+let title = mkSpan({
+  canvas: canvas,
+  w: w,
+  h: 24,
+  top: 8,
+  // left: 0,
+  text: 'Soundflow #4 - Justin Yang',
+  fontSize: 18,
+  color: 'rgb(153,255,0)'
+});
+let titleW = title.getBoundingClientRect().width;
+let titlePosX = center - (titleW / 2);
+title.style.left = titlePosX.toString() + 'px';
+// </editor-fold>
+
+// <editor-fold> Piece ID Caption
+let pieceIDinstructions = mkSpan({
+  canvas: canvas,
+  top: 50,
+  left: 15,
+  text: 'Please enter an ID for this performance:',
+  fontSize: 16,
+  color: 'white'
+});
+// </editor-fold> END Piece ID Caption
+
+//<editor-fold> PieceID Input Field
+let pieceIDinput = mkInputField({
+  canvas: canvas,
+  id: 'pieceIDinput',
+  w: 90,
+  h: 26,
+  top: 43,
+  left: 302,
+  color: 'black',
+  fontSize: 22
+});
+// </editor-fold> END PieceID Input Field
+
+// <editor-fold> Select Parts Caption
+let selectPartsCaption = mkSpan({
+  canvas: canvas,
+  top: 95,
+  left: 15,
+  text: 'Please select the parts to display:',
+  fontSize: 16,
+  color: 'white'
+});
+// </editor-fold> END Select Parts Caption
+
+// <editor-fold> Select Parts Checkboxes
+let selectPartsCBs = mkCheckboxesHoriz({
+  canvas:canvas,
+  numBoxes: 5,
+  boxSz: 25,
+  gap: 20,
+  top: 122,
+  left: 20,
+  lblArray: ['1','2','3','4', '5'],
+  lblClr: 'rgb(153,255,0)'
+});
+// </editor-fold> END Select Parts Checkboxes
--- server.js
+++ server.js
@@ -1,36 +1,52 @@
+//<editor-fold> LIBRARIES
 var express = require('express');
 var app = express();
 var path = require('path');
 var timesyncServer = require('timesync/server');
-var server = require('http').createServer(app);
-io = require('socket.io').listen(server);
+var httpServer = require('http').createServer(app);
+io = require('socket.io').listen(httpServer);
 const fs = require('fs');
+//</editor-fold> END LIBRARIES
+
+// set up port
 const PORT = process.env.PORT || 5000
+
+//<editor-fold> SERVE STATIC FILES THROUGH EXPRESS
 app.use(express.static(path.join(__dirname, '/public')));
-app.use(express.static(path.join(__dirname, '/private')));
 app.get('/', function(req, res) {
-  // res.sendFile(path.join(__dirname, '/public/index.html'));
-  res.sendFile(path.join(__dirname, '/public/pieces/sf004/sf004.html'));
+  res.sendFile(path.join(__dirname, '/public/pieces/sf004/sf004_launchPage.html'));
 });
-server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
-// TIMESYNC SERVER -------------- >
+//</editor-fold> END SERVER STATIC FILES
+
+//Initialize http Server
+httpServer.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+
+//<editor-fold> TIMESYNC SERVER
 app.use('/timesync', timesyncServer.requestHandler);
-//<editor-fold> << SOCKET IO >> -------------------------------------------- //
+//</editor-fold> END TIMESYNC SERVER
+
+
+//<editor-fold> SOCKET IO
 io.on('connection', function(socket) {
-  //<editor-fold> << sf001 >> --------------------------------------------- //
-  //<editor-fold>  < START PIECE >                         //
+
+  //<editor-fold> START PIECE
   socket.on('sf001_startpiece', function(data) {
     socket.broadcast.emit('sf001_startpiecebroadcast', {});
     socket.emit('sf001_startpiecebroadcast', {});
   });
-  //</editor-fold> END START PIECE END
-  //<editor-fold>  < STOP >                                //
+  //</editor-fold> END START PIECE
+
+
+  //<editor-fold> STOP
   socket.on('sf001_stop', function(data) {
     socket.emit('sf001_stopBroadcast', {});
     socket.broadcast.emit('sf001_stopBroadcast', {});
   });
-  //</editor-fold> END STOP END
-  //<editor-fold>  < PAUSE >                               //
+  //</editor-fold> END STOP
+
+
+  //<editor-fold> PAUSE
   socket.on('sf001_pause', function(data) {
     socket.emit('sf001_pauseBroadcast', {
       pauseState: data.pauseState,
@@ -41,8 +57,10 @@ io.on('connection', function(socket) {
       pauseTime: data.pauseTime
     });
   });
-  //</editor-fold> END PAUSE END
-  //<editor-fold>  < sf001_saveScoreToServer >             //
+  //</editor-fold> END PAUSE
+
+
+  //<editor-fold> SAVE SCORE TO SERVER
   socket.on('sf001_saveScoreToServer', function(data) {
     var fileName = data.pieceData[0];
     var pieceData = data.pieceData[1];
@@ -55,8 +73,10 @@ io.on('connection', function(socket) {
       console.log("The file was saved!");
     });
   });
-  //</editor-fold> END sf001_saveScoreToServer END
-  //<editor-fold>  < LOAD PIECE FROM SERVER >              //
+  //</editor-fold> END SAVE SCORE TO SERVER
+
+
+  //<editor-fold> LOAD PIECE FROM SERVER
   // Request for load piece from splash page
   socket.on('sf001_loadPieceFromServer', function(data) {
     //joining path of directory
@@ -76,142 +96,8 @@ io.on('connection', function(socket) {
       });
     });
   });
-  //</editor-fold> END LOAD PIECE FROM SERVER END
-  //</editor-fold> >> END sf001 END  //////////////////////////////////////////
+  //</editor-fold> END LOAD PIECE FROM SERVER
 
-  //<editor-fold> << SF002 >> --------------------------------------------- //
-  //<editor-fold>  < START PIECE >                         //
-  socket.on('sf002_startpiece', function(data) {
-    socket.broadcast.emit('sf002_startpiecebroadcast', {});
-    socket.emit('sf002_startpiecebroadcast', {});
-  });
-  //</editor-fold> END START PIECE END
-  //<editor-fold>  < START TIME >                          //
-  socket.on('sf002_startTime', function(data) {
-    var newStartTime = data.newStartTime;
-    socket.broadcast.emit('sf002_startTimeBroadcast', {
-      newStartTime: newStartTime
-    });
-    socket.emit('sf002_startTimeBroadcast', {
-      newStartTime: newStartTime
-    });
-  });
-  //</editor-fold> END START TIME END
-  //<editor-fold>  < STOP >                                //
-  socket.on('sf002_stop', function(data) {
-    socket.emit('sf002_stopBroadcast', {});
-    socket.broadcast.emit('sf002_stopBroadcast', {});
-  });
-  //</editor-fold> END STOP END
-  //<editor-fold>  < PAUSE >                               //
-  socket.on('sf002_pause', function(data) {
-    socket.emit('sf002_pauseBroadcast', {
-      pauseState: data.pauseState,
-      pauseTime: data.pauseTime
-    });
-    socket.broadcast.emit('sf002_pauseBroadcast', {
-      pauseState: data.pauseState,
-      pauseTime: data.pauseTime
-    });
-  });
-  //</editor-fold> END PAUSE END
-  //<editor-fold>  < sf002_saveScoreToServer >             //
-  socket.on('sf002_saveScoreToServer', function(data) {
-    var fileName = data.pieceData[0];
-    var pieceData = data.pieceData[1];
-    var pathStr = "/public/pieces/sf002/savedScoreData/" + fileName;
-    var filePath = path.join(__dirname, pathStr);
-    fs.writeFile(filePath, pieceData, function(err) {
-      if (err) {
-        return console.log(err);
-      }
-      console.log("The file was saved!");
-    });
-  });
-  //</editor-fold> END sf002_saveScoreToServer END
-  //<editor-fold>  < LOAD PIECE FROM SERVER >              //
-  // Request for load piece from splash page
-  socket.on('sf002_loadPieceFromServer', function(data) {
-    //joining path of directory
-    const directoryPath = path.join(__dirname, 'public/pieces/sf002/savedScoreData');
-    //passsing directoryPath and callback function
-    fs.readdir(directoryPath, function(err, files) {
-      //handling error
-      if (err) {
-        return console.log('Unable to scan directory: ' + err);
-      }
-      //Send list of files in directory to Splash page
-      socket.broadcast.emit('sf002_loadPieceFromServerBroadcast', {
-        availableScoreData: files
-      });
-      socket.emit('sf002_loadPieceFromServerBroadcast', {
-        availableScoreDataFiles: files
-      });
-    });
-  });
-  //</editor-fold> END LOAD PIECE FROM SERVER END
-  //</editor-fold> >> END SF002 END  //////////////////////////////////////////
 
-  //<editor-fold> << sf003 >> --------------------------------------------- //
-  //<editor-fold>  < START PIECE >                         //
-  socket.on('sf003_startpiece', function(data) {
-    socket.broadcast.emit('sf003_startpiecebroadcast', {});
-    socket.emit('sf003_startpiecebroadcast', {});
-  });
-  //</editor-fold> END START PIECE END
-  //<editor-fold>  < STOP >                                //
-  socket.on('sf003_stop', function(data) {
-    socket.emit('sf003_stopBroadcast', {});
-    socket.broadcast.emit('sf003_stopBroadcast', {});
-  });
-  //</editor-fold> END STOP END
-  //<editor-fold>  < PAUSE >                               //
-  socket.on('sf003_pause', function(data) {
-    socket.emit('sf003_pauseBroadcast', {
-      pauseState: data.pauseState,
-      pauseTime: data.pauseTime
-    });
-    socket.broadcast.emit('sf003_pauseBroadcast', {
-      pauseState: data.pauseState,
-      pauseTime: data.pauseTime
-    });
-  });
-  //</editor-fold> END PAUSE END
-  //<editor-fold>  < sf003_saveScoreToServer >             //
-  socket.on('sf003_saveScoreToServer', function(data) {
-    var fileName = data.pieceData[0];
-    var pieceData = data.pieceData[1];
-    var pathStr = "/public/pieces/sf003/savedScoreData/" + fileName;
-    var filePath = path.join(__dirname, pathStr);
-    fs.writeFile(filePath, pieceData, function(err) {
-      if (err) {
-        return console.log(err);
-      }
-      console.log("The file was saved!");
-    });
-  });
-  //</editor-fold> END sf003_saveScoreToServer END
-  //<editor-fold>  < LOAD PIECE FROM SERVER >              //
-  // Request for load piece from splash page
-  socket.on('sf003_loadPieceFromServer', function(data) {
-    //joining path of directory
-    const directoryPath = path.join(__dirname, 'public/pieces/sf003/savedScoreData');
-    //passsing directoryPath and callback function
-    fs.readdir(directoryPath, function(err, files) {
-      //handling error
-      if (err) {
-        return console.log('Unable to scan directory: ' + err);
-      }
-      //Send list of files in directory to Splash page
-      socket.broadcast.emit('sf003_loadPieceFromServerBroadcast', {
-        availableScoreData: files
-      });
-      socket.emit('sf003_loadPieceFromServerBroadcast', {
-        availableScoreDataFiles: files
-      });
-    });
-  });
-  //</editor-fold> END LOAD PIECE FROM SERVER END
-  //</editor-fold> >> END sf003 END  //////////////////////////////////////////
 }); // End Socket IO
-//</editor-fold> >> END SOCKET IO END  ////////////////////////////////////////
+//</editor-fold> >> END SOCKET IO
